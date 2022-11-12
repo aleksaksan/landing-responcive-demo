4b1e3faf65e69cf9d7e58a158917690710be3de3
Aleksander <sanyok.10rus@gmail.com>
2022-11-13 02:43:20 +0400

    it was made menu for mobile

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const burgerMenu = document.querySelector(".header-burger");
+const burgerMenu = document.querySelector(".header-burger-btn");
 
 burgerMenu.addEventListener('click', () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("active");
   burgerMenu.classList.toggle("active");
 });
